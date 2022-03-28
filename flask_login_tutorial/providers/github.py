fa4305b64dcb4e061e5bc9a30a94f117555c632c
Toan Bui Song <toanbs@fpt.com.vn>
2022-03-28 21:54:07 +0700

    .

---
+++ flask_login_tutorial/providers/github.py
@@ -0,0 +1,49 @@
+from flask import request, session, redirect, url_for, current_app
+
+from .base import authlib_oauth_client
+
+
+settings = {
+    'github_oauth_key': '',
+    'github_oauth_secret': '',
+    'github_oauth_scope': 'user:repo',
+    'github_oauth_api_url': 'https://api.github.com/',
+    'github_oauth_token_url': 'https://github.com/login/oauth/access_token',
+    'github_oauth_authorize_url': 'https://github.com/login/oauth/',
+}
+
+
+def github_oauth():
+
+    def fetch_github_token():
+        return session.get('github_token')
+
+    def update_token(token):
+        session['github_token'] = token
+        return token
+
+    github = authlib_oauth_client.register(
+        'github',
+        client_id=settings.get('github_oauth_key'),
+        client_secret=settings.get('github_oauth_secret'),
+        request_token_params={'scope': settings.get('github_oauth_scope')},
+        api_base_url=settings.get('github_oauth_api_url'),
+        request_token_url=None,
+        access_token_url=settings.get('github_oauth_token_url'),
+        authorize_url=settings.get('github_oauth_authorize_url'),
+        client_kwargs={'scope': settings.get('github_oauth_scope')},
+        fetch_token=fetch_github_token,
+        update_token=update_token)
+
+    @current_app.route('/github/authorized')
+    def github_authorized():
+        session['github_oauthredir'] = url_for('.github_authorized',
+                                               _external=True)
+        token = github.authorize_access_token()
+        if token is None:
+            return 'Access denied: reason=%s error=%s' % (
+                request.args['error'], request.args['error_description'])
+        session['github_token'] = (token)
+        return redirect(url_for('index.login'))
+
+    return github
